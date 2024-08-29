929bd535f358f875506ce1339f4f9309ce2d4d9d
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-29 21:31:21 +0700

    refactor: change tabs to spaces

--- task1/Graph.java
+++ task1/Graph.java
@@ -2,64 +2,64 @@ import java.util.*;
 import java.io.*;
 
 public class Graph {
-	private final Queue<File> queue = new ArrayDeque<>();;
-	private final Map<File, Color> coloredGraph = new HashMap<>();
-	public final Map<File, ArrayList<File>> graph = new HashMap<>();
+  private final Queue<File> queue = new ArrayDeque<>();;
+  private final Map<File, Color> coloredGraph = new HashMap<>();
+  public final Map<File, ArrayList<File>> graph = new HashMap<>();
 
-	private static enum Color {
-		BLACK, GRAY, WHITE
-	}
+  private static enum Color {
+    BLACK, GRAY, WHITE
+  }
 
-	private boolean dfs(File vertex) {
-		Color currentColor = coloredGraph.get(vertex);
+  private boolean dfs(File vertex) {
+    Color currentColor = coloredGraph.get(vertex);
 
-		switch (currentColor) {
-			case BLACK:
-				return true;
-			case GRAY:
-				return false;
-			case WHITE:
-				coloredGraph.put(vertex, Color.GRAY);
+    switch (currentColor) {
+      case BLACK:
+        return true;
+      case GRAY:
+        return false;
+      case WHITE:
+        coloredGraph.put(vertex, Color.GRAY);
 
-				ArrayList<File> neighbours = graph.get(vertex);
-				for (File neighbour : neighbours) {
-					if (!dfs(neighbour)) {
-						return false;
-					}
-				}
-		}
-		coloredGraph.put(vertex, Color.BLACK);
-		queue.add(vertex);
-		return true;
-	}
+        ArrayList<File> neighbours = graph.get(vertex);
+        for (File neighbour : neighbours) {
+          if (!dfs(neighbour)) {
+            return false;
+          }
+        }
+    }
+    coloredGraph.put(vertex, Color.BLACK);
+    queue.add(vertex);
+    return true;
+  }
 
-	private boolean hasCycle() {
-		ArrayList<File> keys = new ArrayList<>(graph.keySet());
-		keys.sort(Comparator.comparing(File::getName));
+  private boolean hasCycle() {
+    ArrayList<File> keys = new ArrayList<>(graph.keySet());
+    keys.sort(Comparator.comparing(File::getName));
 
-		for (File key : keys) {
-			if (!dfs(key)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    for (File key : keys) {
+      if (!dfs(key)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-	public void addVertex(File key, ArrayList<File> values) {
-		graph.putIfAbsent(key, values);
-		coloredGraph.putIfAbsent(key, Color.WHITE);
-	}
+  public void addVertex(File key, ArrayList<File> values) {
+    graph.putIfAbsent(key, values);
+    coloredGraph.putIfAbsent(key, Color.WHITE);
+  }
 
-	public ArrayList<File> getSortedGraph() {
-		if (hasCycle()) {
-			throw new RuntimeException("Cycle detected");
-		}
+  public ArrayList<File> getSortedGraph() {
+    if (hasCycle()) {
+      throw new RuntimeException("Cycle detected");
+    }
 
-		ArrayList<File> sortedGraph = new ArrayList<>();
-		while (!queue.isEmpty()) {
-			sortedGraph.add(queue.remove());
-		}
+    ArrayList<File> sortedGraph = new ArrayList<>();
+    while (!queue.isEmpty()) {
+      sortedGraph.add(queue.remove());
+    }
 
-		return sortedGraph;
-	}
+    return sortedGraph;
+  }
 }
--- task1/Main.java
+++ task1/Main.java
@@ -5,55 +5,55 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 class Main {
-	private static final Path ROOT_PATH = Paths.get("").toAbsolutePath();
-	private static final Pattern PATTERN_REQUIRE = Pattern.compile("require '([^']+)'");;
-	private static final Graph graph = new Graph();;
-	private static final FileManager FileManager = new FileManager(ROOT_PATH);
+  private static final Path ROOT_PATH = Paths.get("").toAbsolutePath();
+  private static final Pattern PATTERN_REQUIRE = Pattern.compile("require '([^']+)'");;
+  private static final Graph graph = new Graph();;
+  private static final FileManager FileManager = new FileManager(ROOT_PATH);
 
-	private final static void searchDependencies(String line, ArrayList<File> dependencies) {
-		Matcher matcher = PATTERN_REQUIRE.matcher(line);
+  private final static void searchDependencies(String line, ArrayList<File> dependencies) {
+    Matcher matcher = PATTERN_REQUIRE.matcher(line);
 
-		while (matcher.find()) {
-			String foundPath = matcher.group(1);
-			Path relativeFilePath = Paths.get(foundPath);
-			Path absoluteFilePath = ROOT_PATH.resolve(relativeFilePath);
-			File txtFile = new File(absoluteFilePath + ".txt");
+    while (matcher.find()) {
+      String foundPath = matcher.group(1);
+      Path relativeFilePath = Paths.get(foundPath);
+      Path absoluteFilePath = ROOT_PATH.resolve(relativeFilePath);
+      File txtFile = new File(absoluteFilePath + ".txt");
 
-			dependencies.add(txtFile);
-		}
-	}
+      dependencies.add(txtFile);
+    }
+  }
 
-	private final static void addFileToGraph(File file) {
-		ArrayList<File> dependencies = new ArrayList<>();
+  private final static void addFileToGraph(File file) {
+    ArrayList<File> dependencies = new ArrayList<>();
 
-		try (BufferedReader reader = new BufferedReader(new FileReader(file.getAbsolutePath()))) {
-			String line;
-			while ((line = reader.readLine()) != null) {
-				searchDependencies(line, dependencies);
-			}
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
+    try (BufferedReader reader = new BufferedReader(new FileReader(file.getAbsolutePath()))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        searchDependencies(line, dependencies);
+      }
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
 
-		graph.addVertex(file, dependencies);
-	}
+    graph.addVertex(file, dependencies);
+  }
 
-	public static void main(String[] args) {
-		FileManager.searchFiles(ROOT_PATH);
-		ArrayList<File> files = FileManager.getFiles();
+  public static void main(String[] args) {
+    FileManager.searchFiles(ROOT_PATH);
+    ArrayList<File> files = FileManager.getFiles();
 
-		for (File file : files) {
-			addFileToGraph(file);
-		}
+    for (File file : files) {
+      addFileToGraph(file);
+    }
 
-		try {
-			ArrayList<File> sortedFiles = graph.getSortedGraph();
-			File resultFile = FileManager.createFile("result");
-			FileManager.concatFiles(resultFile, sortedFiles);
+    try {
+      ArrayList<File> sortedFiles = graph.getSortedGraph();
+      File resultFile = FileManager.createFile("result");
+      FileManager.concatFiles(resultFile, sortedFiles);
 
-			System.out.println("Check the result.txt in the root folder");
-		} catch (RuntimeException e) {
-			System.out.println(e.getMessage());
-		}
-	}
+      System.out.println("Check the result.txt in the root folder");
+    } catch (RuntimeException e) {
+      System.out.println(e.getMessage());
+    }
+  }
 }
