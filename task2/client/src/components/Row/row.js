33ed2c523e33ca21bf6d3b488d570cd2a514fd79
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-31 02:26:20 +0700

    client/refactor: add Form component

--- task2/client/src/components/Row/row.js
+++ task2/client/src/components/Row/row.js
@@ -1,5 +1,4 @@
-import Button from "../Button/button";
-import Input from "../Input/input";
+import Form from "../Form/form";
 
 export default class Row {
   /**
@@ -8,92 +7,44 @@ export default class Row {
    * @param {((student: Student) => void) | null} onConfirmClick
    */
   constructor(data, onRowClick, onConfirmClick) {
-    this.inputs = [];
     this.element = this.#getRow(data);
 
-    onRowClick && this.element.addEventListener("click", onRowClick);
-    onConfirmClick && this.#setConfirmButton(this.element, onConfirmClick);
+    if (data) {
+      const content = this.#getRowContent(data);
+      this.element.appendChild(content);
+      onRowClick && this.element.addEventListener("click", onRowClick);
+    } else {
+      const form = new Form(onConfirmClick);
+      this.element.appendChild(form.element);
+    }
   }
 
   /**
-   * Creates and fills a new row
-   * @param {Student | null} data
+   * Creates and row HTML element
    * @returns {HTMLDivElement}
    */
-  #getRow(data) {
+  #getRow() {
     const row = document.createElement("div");
-    const ul = document.createElement("ul");
-
     row.classList.add("row");
-    ul.classList.add("row__ul");
-    row.appendChild(ul);
-
-    data ? this.#fillData(data, ul) : this.#fillInputs(ul);
     return row;
   }
 
   /**
-   * Fills row with student's data
+   * Fills row with data and returns HTML element
    * @param {Student} data
-   * @param {HTMLUListElement} list
+   * @returns {HTMLUListElement}
    */
-  #fillData(data, list) {
+  #getRowContent(data) {
+    const ul = document.createElement("ul");
+    ul.classList.add("row__ul");
+
     for (const key in data) {
       const li = document.createElement("li");
       li.textContent = data[key];
-      list.append(li);
+      ul.append(li);
     }
-  }
 
-  /**
-   * Fills row with empty inputs.
-   * @param {HTMLUListElement} list
-   */
-  #fillInputs(list) {
-    for (let i = 0; i < 6; ++i) {
-      const li = document.createElement("li");
-      list.append(li);
-
-      if (!i) continue;
-
-      const input = new Input();
-      this.inputs.push(input);
-      li.append(input.element);
-    }
-  }
-
-  /**
-   * Creates a button for adding a new student
-   * @param {HTMLDivElement} row
-   * @param {(data: String[]) => void} onConfirmClick
-   */
-  #setConfirmButton(row, onConfirmClick) {
-    const button = new Button("Confirm", () =>
-      this.#handleConfirm(onConfirmClick)
-    );
-    row.append(button.element);
-  }
-
-  /**
-   * Checks correctness of the inputs before calling the confirm handler
-   * @param {(data: String[]) => void} onConfirmClick
-   */
-  #handleConfirm(onConfirmClick) {
-    let allFilled = true;
-
-    this.inputs.forEach((input) => {
-      if (input.isEmpty()) {
-        input.setError();
-        allFilled = false;
-      } else {
-        input.unsetError();
-      }
-    });
-
-    if (allFilled) {
-      const data = this.inputs.map((input) => input.value);
-      onConfirmClick(data);
-    }
+    return ul;
   }
 }
 
