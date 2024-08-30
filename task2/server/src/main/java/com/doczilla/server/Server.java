b14ec45af6a5798978746ea2099f4fc5cef46351
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-30 18:20:13 +0700

    server/refactor: update Server with Student entities

--- task2/server/src/main/java/com/doczilla/server/Server.java
+++ task2/server/src/main/java/com/doczilla/server/Server.java
@@ -1,5 +1,8 @@
 package com.doczilla.server;
 
+import com.doczilla.controller.StudentController;
+import com.doczilla.repository.StudentRepository;
+import com.doczilla.service.StudentService;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -9,6 +12,13 @@ import java.net.InetSocketAddress;
 public class Server {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        StudentRepository studentRepository = new StudentRepository();
+        StudentService studentService = new StudentService(studentRepository);
+        StudentController studentController = new StudentController(studentService);
+
+        server.createContext("/students", studentController);
+
         server.setExecutor(null);
         System.out.println("Server is running on port " + 8000);
         server.start();
