03652201c4846505e2accc039f798e6a578dde7d
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-30 14:25:27 +0700

    client/feat: add header doc and delete button

--- task2/client/src/components/Header/header.js
+++ task2/client/src/components/Header/header.js
@@ -4,16 +4,36 @@ export default class Header {
   constructor() {
     this.element = this.getHeader();
 	}
+
+	/**
+	 * Creates a Delete button element
+	 * @returns {HTMLDivElement}
+	*/
+	getDeleteButton() {
+		const button = document.createElement('div');
+		button.classList.add('header__button_delete', 'hidden');
+
+		const img = document.createElement('img');
+		img.src = '';
+
+		button.appendChild(img);
+		return button;
+	}
 	
+	/**
+	 * Creates a Header element
+	 * @returns {HTMLDivElement}
+	*/
   getHeader() {
     const header = document.createElement("div");
 		const h1 = document.createElement("h1");
+		const deleteButton = this.getDeleteButton();
 		
 		header.classList.add("header");
-    h1.classList.add("header__title");
+		h1.classList.add("header__title");
 
     h1.textContent = "Student class";
-		header.appendChild(h1);
+		header.append(h1, deleteButton);
 		
     return header;
   }
