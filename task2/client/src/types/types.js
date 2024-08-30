2f1649ae40bd8e0783c73e4211591f0967ad12e4
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-30 20:42:19 +0700

    client/feat: add Student type for docs

---
+++ task2/client/src/types/types.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} Student
+ * @property {number} id
+ * @property {string} name
+ * @property {string} surname
+ * @property {string} patronymic
+ * @property {string} datebirth
+ * @property {number} group
+ */
+
