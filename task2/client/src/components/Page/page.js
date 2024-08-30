92f0746776db0885c94791c4e89b0abb6c828dc6
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-30 20:44:22 +0700

    client/feat: add callbacks to Page class parameters

--- task2/client/src/components/Page/page.js
+++ task2/client/src/components/Page/page.js
@@ -1,56 +1,34 @@
 import Header from "../Header/header";
 import Table from "../Table/table";
 
-const students = [
-  {
-    id: 1,
-    name: "Sofya",
-    surname: "Sofya",
-    patronymic: "Patronomic",
-    datebirth: "11.11.2000",
-    group: 10,
-  },
-  {
-    id: 2,
-    name: "Ivan",
-    surname: "Ivanov",
-    patronymic: "Ivanovich",
-    datebirth: "01.01.2001",
-    group: 10,
-  },
-  {
-    id: 3,
-    name: "Anna",
-    surname: "Petrova",
-    patronymic: "Petrovna",
-    datebirth: "02.02.2002",
-    group: 11,
-  },
-];
-
 export default class Page {
   #header;
   #table;
-	element;
-	
-  constructor() {
-		this.#header = new Header();
-		this.#table = new Table(students, this.handleAddClick);
+  element;
+
+  /**
+   * @param {Student[] | []} students
+   * @param {() => {}} addCallback
+   * @param {() => {}} deleteCallback
+   */
+  constructor(students, addCallback, deleteCallback) {
+    this.#header = new Header(deleteCallback);
+    this.#table = new Table(students, addCallback);
     this.element = this.getPage();
-	}
+  }
 
-	handleAddClick() {
-		
-	}
-
+  /**
+   * Creates a page HTML element
+   * @returns {HTMLDivElement}
+   */
   getPage() {
-    const page = document.createElement("div");		
-		const pageContent = document.createElement("div");
+    const page = document.createElement("div");
+    const pageContent = document.createElement("div");
 
-		page.classList.add("page");
-		pageContent.classList.add("page__content");
+    page.classList.add("page");
+    pageContent.classList.add("page__content");
 
-		pageContent.append(this.#table.element);
+    pageContent.append(this.#table.element);
     page.append(this.#header.element, pageContent);
     return page;
   }
--- task2/client/src/index.js
+++ task2/client/src/index.js
@@ -1,11 +1,48 @@
 import "./index.scss";
 
+import "./types/types";
 import Page from "./components/Page/page";
+import ApiRequests from "./api/apiRequests";
 
-function main() {
-  const page = new Page().element;
-  document.body.appendChild(page);
+class Main {
+  constructor() {
+    this.api = new ApiRequests();
+  }
+
+  /**
+   * Handle click on add button
+   * @returns {Student[]}
+   */
+  handleAddClick() {
+    this.api.postStudent(student);
+  }
+
+  /**
+   * Handle click on delete button
+   * @returns {Student[]}
+   */
+  handleDeleteClick() {}
+
+  /**
+   * Returns all students
+   * @returns {Student[] | []}
+   */
+  getStudents() {
+    const students = this.api.getAllStudents();
+    return students;
+  }
+
+  main() {
+    const students = this.getStudents();
+    const page = new Page(
+      students,
+      this.handleAddClick,
+      this.handleDeleteClick
+    );
+    document.body.appendChild(page.element);
+  }
 }
 
-main();
+const main = new Main();
+main.main();
 
