00c349f918250e80b07b62e8a3a24b043c0468de
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-30 18:18:08 +0700

    server/refactor: delete unused code in App.java

--- task2/server/src/main/java/com/doczilla/App.java
+++ task2/server/src/main/java/com/doczilla/App.java
@@ -4,18 +4,7 @@ import java.io.IOException;
 
 import com.doczilla.server.Server;
 
-/**
- * Hello world!
- */
 public final class App {
-    private App() {
-    }
-
-    /**
-     * Says hello to the world.
-     *
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
         try {
             Server.main(args);
