5e2fac5d5b35b25293ddde6acca09c031a1a01dd
Sofya <s.cheboltasova@g.nsu.ru>
2024-08-31 21:11:23 +0700

    task2/fix: fix docker bugs

--- task2/server/src/main/java/com/doczilla/config/DatabaseConfig.java
+++ task2/server/src/main/java/com/doczilla/config/DatabaseConfig.java
@@ -3,9 +3,10 @@ package com.doczilla.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConfig {
-    private static final String URL = "jdbc:postgresql://localhost:5432/doczillatask2";
+    private static final String URL = "jdbc:postgresql://host.docker.internal:5432/doczillatask2";
     private static final String USER = "postgres";
     private static final String PASSWORD = "Sofya2002";
 
@@ -15,6 +16,24 @@ public class DatabaseConfig {
         } catch (ClassNotFoundException e) {
             throw new SQLException("PostgreSQL Driver not found", e);
         }
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS Student ("
+                + "id SERIAL PRIMARY KEY, "
+                + "name VARCHAR(255) NOT NULL, "
+                + "surname VARCHAR(255) NOT NULL, "
+                + "patronymic VARCHAR(255) NOT NULL, "
+                + "birthdate DATE NOT NULL, "
+                + "groupid INTEGER NOT NULL);";
+
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute(createTableSQL);
+            System.out.println("Table 'Student' checked/created successfully!");
+        } catch (SQLException e) {
+            throw new SQLException("Error creating table 'Student'", e);
+        }
+
+        return connection;
     }
 }
